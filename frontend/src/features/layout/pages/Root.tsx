b8d5a52239910a0451732c96c60a3aaaa59c5a6a
hugopiramide <hugocastillo.deus@gmail.com>
2026-05-19 09:27:09 +0200

    feature(front): Add component to root structure

--- frontend/src/features/layout/pages/Root.tsx
+++ frontend/src/features/layout/pages/Root.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Breadcrumbs from '../components/Breadcrumbs/Breadcrumbs';
 
 const Root = () => {
     return(
         <>
             <Navbar />
+            <Breadcrumbs />
             <main>
                 <Outlet />
             </main>
